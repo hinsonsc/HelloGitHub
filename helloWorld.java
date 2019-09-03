3884fe7d0c5caa7edea858b58d2084ac597d23b7
Seth Hinson <hinsonsc@users.noreply.github.com>
2019-09-03 02:01:07 -0400

    Create helloWorld.java

---
+++ helloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * Class for HW 1 for CSCI 360
+ *
+ * @author Seth Hinson
+ * @version 1.0
+ */
+public class helloworld
+{
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World!"); 
+    } 
+}
